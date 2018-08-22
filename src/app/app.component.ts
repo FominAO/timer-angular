212706b1cd82be36faee914d9a3f9c4d2d50ce9d
FominMajor <fominaomajor@gmail.com>
2018-08-22 16:06:45 +0300

    readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  clocks= [1,22,56,0]
+  circleColor = "#d82"
+  circleWidth = "300"
+  circleStrokeWidth = "8px"
+  textMatr = ["matrix(1 0 0 1 180 340)", "matrix(1 0 0 1 120 340)", "matrix(1 0 0 1 120 340)", "matrix(1 0 0 1 180 340)"]
+  changeColor(e) {
+    if ((e.length == 3) || (e.length == 6)) {
+    this.circleColor = "#" + e;
+  }
+  }
+  changeSW(e) {
+    if (+e < 50) {
+    this.circleStrokeWidth = e + "px"
+  }
+  }
 }
+
+
