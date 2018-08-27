f51d4468f67241b227347455089db4d2a8b1091a
FominMajor <fominaomajor@gmail.com>
2018-08-27 16:25:47 +0300

    added GetCODE function, style polishing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Ticking } from './ticking/ticking.directive';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  timerLabel = 'Label'
+  getCode = false;
+  customCode:string = "";
   customDate:Date;
   title = 'app';
   currentDate:Date;
@@ -19,8 +22,8 @@ export class AppComponent implements OnInit{
   stroked;
   strokedRed:string;
   circleStrokeWidth = "8"
-  twoNumberMatr = "matrix(1 0 0 1 120 340)"
-  oneNumberMatr = "matrix(1 0 0 1 180 340)";
+  twoNumberMatr = "matrix(1, 0, 0, 1, 120, 340)"
+  oneNumberMatr = "matrix(1, 0, 0, 1, 180, 340)";
   textMatr = ["matrix(1 0 0 1 180 340)", "matrix(1 0 0 1 120 340)", "matrix(1 0 0 1 120 340)", "matrix(1 0 0 1 180 340)"]
   changeColor(e) {
     
@@ -36,7 +39,7 @@ export class AppComponent implements OnInit{
     this.currentDate = new Date();
     this.newDate = this.currentDate;
     this.maxDate = new Date(+(this.currentDate) + 8550600000);
-    this.customDate = new Date(+(this.currentDate) + 86400000);
+    this.customDate = new Date(+(this.currentDate) + 84600000);
     
   }
   tick() {
@@ -120,7 +123,21 @@ export class AppComponent implements OnInit{
         this.textMatr[i] = this.twoNumberMatr
        }
     }
-  } 
+  }
+  touchSlider (e) {
+    console.log('event: ', e.value)
+  }
+  setLabel(e) {
+    this.timerLabel = e;
+  }
+  set3() {
+    console.log(document.getElementsByClassName('counter'))
+    document.getElementsByClassName('st1')[1].innerHTML = '3'
+  }
+  buildCode () {
+    this.getCode = true;
+    this.customCode = `<html><head><style>.timer{font-family: Arial, Helvetica, sans-serif;display: flex;flex-wrap: wrap;justify-content: space-between}.timer__label{width: 100%;height: 10%;text-align: center;font-size: 10vw;}.timer__clocks {height: 90%;width: 24%;}.timer-circle{transform: translateY(500px) rotate(-90deg) scaleY(-1) translateY(-500px);transition: 500ms;}</style></head><body><div class="timer"><div class="timer__label">${this.timerLabel}</div><div class="timer__clocks" title="Days"><div class="svg-circle-container"><svg class="svg-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"> <style type="text/css"> .st1{fill: #000000; font-family: "Arial"; font-size: 250px;} </style><circle  class="timer-circle" cx="250" cy="250" r="240" fill="none" stroke="#${this.circleColor}" stroke-width="${this.circleStrokeWidth}"/><text transform="${this.oneNumberMatr}" class="st1">0</text></svg></div></div><div class="timer__clocks" title="Hours"><div class="svg-circle-container"><svg class="svg-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><style type="text/css"> .st1{fill: #000000; font-family: "Arial"; font-size: 250px;}</style><circle  class="timer-circle" cx="250" cy="250" r="240" fill="none" stroke="#${this.circleColor}" stroke-width="${this.circleStrokeWidth}"/><text transform="${this.oneNumberMatr}" class="st1">0</text></svg></div></div><div class="timer__clocks" title="Minutes"><div class="svg-circle-container"><svg class="svg-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"> <style type="text/css"> .st1{fill: #000000; font-family: "Arial"; font-size: 250px;}</style><circle  class="timer-circle" cx="250" cy="250" r="240" fill="none" stroke="#${this.circleColor}" stroke-width="${this.circleStrokeWidth}"/><text transform="${this.oneNumberMatr}" class="st1">0</text></svg></div></div><div class="timer__clocks" title="Seconds"><div class="svg-circle-container"><svg class="svg-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"> <style type="text/css"> .st1{fill: #000000; font-family: "Arial"; font-size: 250px;}</style><circle  class="timer-circle" cx="250" cy="250" r="240" fill="none" stroke="#${this.circleColor}" stroke-width="${this.circleStrokeWidth}"/><text transform="${this.oneNumberMatr}" class="st1">0</text></svg></div></div></div><script type="text/javascript">class Timer {constructor(goalDate){this.goal = goalDate;this.data = {"days": 0,"hours": 0,"minutes": 0,"seconds": 0}};timer() {let currentDate = new Date();let newDate = this.goal - +currentDate;let seconds = Math.round(+newDate / 1000);let minutes = Math.round((seconds + 30)  / 60);let hours = Math.round((minutes + 30) / 60);let days =  Math.round((hours + 12) / 24);seconds = seconds % 60;minutes = minutes % 60;hours = hours % 24;this.data={"days": days,"hours": hours,"minutes": minutes,"seconds": seconds};return this.data};tick() {let data = this.timer(); let datePart = document.getElementsByClassName("st1"); let dateCircles = document.getElementsByClassName("timer-circle"); var i = 0; for (let key in data) {if (datePart[i].innerHTML != data[key]){datePart[i].innerHTML = data[key]; if (data[key] > 9) {datePart[i].style.transform = "${this.twoNumberMatr}"} else {datePart[i].style.transform = "${this.oneNumberMatr}"} if (key == "hours") {dateCircles[i].style.strokeDasharray = 1508 - Math.round(62.83*data[key])+", 1508"} else {dateCircles[i].style.strokeDasharray = 1508 - Math.round(25.13*data[key])+", 1508"}}i++;}}start() {setInterval(this.tick.bind(this), 1000)}}let timer = new Timer(${+this.customDate});timer.start()</script></body></html>`
+  }
 }
 
 
